dc6cabdd2d4ca0c212191c54a4b6d2250d8ffb6b
ximenaortegam2605@gmail.com <laurameliza26@gmail.com>
2023-08-10 09:41:00 -0500

    ingresando datos

---
+++ scripts/scripst.1.js
@@ -0,0 +1,5 @@
+let nombre = prompt("ingrese su nombre");
+let apellido = prompt("ingrese su apellido");
+let cedula = prompt("ingrese su cedula");
+let edad = prompt("ingrese su edad");
+console.log  ("La informacion del usuario es" + nombre + apellido + cedula + edad);

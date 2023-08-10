a98fe6a417504e6f9e3a94636e7384be0b2012db
machadoalejo <machadoalejo@gmail.com>
2023-08-10 09:57:39 -0500

    Mod Alejandro

---
+++ scripts/script3.js
@@ -0,0 +1,9 @@
+const firstname = prompt ("Ingresar primer nombre")
+const secondname = prompt ("Ingresar segundo nombre")
+const age = parseInt(prompt("Ingresar edad"))
+const numberPhone = parseInt(prompt("Ingrese numero de telefono"))
+
+console.log (`Primer nombre: ${firstname}`)
+console.log (`Segundo nombre: ${secondname}`)
+console.log (`Tiene  ${age} años`)
+console.log (`Su numero de celular es:  ${firstname}`)
